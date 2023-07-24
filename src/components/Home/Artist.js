bc1bbf7c75ff2c1954ca871b274c21130a25b95a
unliusizeg <ezgiunlus@icloud.com>
2023-07-24 11:51:06 +0300

    ikon yerleştirildi home page'e başlık oluşturuldu

--- src/components/Home/Artist.js
+++ src/components/Home/Artist.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import {BsFillMoonFill} from "react-icons/bs"
 
 const Artist = () => {
   return (
-    <div>Artist</div>
+    <div className='flex flex-col justify-center items-center mt-20'>
+      <div >
+      <BsFillMoonFill className='w-8 h-8 ' />
+      </div>
+      <div className='flex justify-center items-center my-auto rounded-lg mt-4 bg-orange-500 text-white w-36  h-12 from-slate-950'>Top Artist List</div>
+    </div>
   )
 }
 
